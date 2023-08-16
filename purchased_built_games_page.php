3a58dc18118c6e23497db6f4813b5c81bd9d10b9
denzu-go <73340375+denzu-go@users.noreply.github.com>
2023-08-16 09:45:36 +0800

    ey

--- purchased_built_games_page.php
+++ purchased_built_games_page.php
@@ -33,15 +33,19 @@ while ($game = mysqli_fetch_assoc($result)) {
     echo 'Is Published: ' . ($game['is_published'] == 1 ? 'Yes' : 'No') . '<br>';
     echo 'Price: $' . $game['price'] . '<br>';
 
-    // Add "Publish" button
-    echo '<form method="post" action="edit_game_page.php">';
-    echo '<input type="hidden" name="built_game_id" value="' . $game['built_game_id'] . '">';
-    echo '<input type="hidden" name="user_id" value="' . $game['creator_id'] . '">'; // Pass the user ID
-    echo '<input type="hidden" name="game_id" value="' . $game['game_id'] . '">'; // Pass the game ID
-    echo '<input type="hidden" name="game_name" value="' . $game['name'] . '">'; // Pass the game name
-    echo '<input type="hidden" name="price" value="' . $game['price'] . '">'; // Pass the price
-    echo '<button type="submit" name="publish">Publish</button>';
-    echo '</form>';
+    if ($game['is_published'] == 0) {
+        // Add "Publish" button only if the game is not already published
+        echo '<form method="post" action="edit_game_page.php">';
+        echo '<input type="hidden" name="built_game_id" value="' . $game['built_game_id'] . '">';
+        echo '<input type="hidden" name="user_id" value="' . $game['creator_id'] . '">'; // Pass the user ID
+        echo '<input type="hidden" name="game_id" value="' . $game['game_id'] . '">'; // Pass the game ID
+        echo '<input type="hidden" name="game_name" value="' . $game['name'] . '">'; // Pass the game name
+        echo '<input type="hidden" name="price" value="' . $game['price'] . '">'; // Pass the price
+        echo '<button type="submit" name="publish">Publish</button>';
+        echo '</form>';
+    } else {
+        echo 'Already Published';
+    }
 
     echo '</li>';
 }
